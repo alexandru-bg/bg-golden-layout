ceee842e571085e9eb7711602a2b6ec64bab842c
Alexandru Constantin <alexandru.constantin@brokergenius.com>
2019-02-08 16:23:20 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-ng build --prod --base-href "https://alexanfru-bg.github.io/bg-golden-layout/"
+ng build --prod --base-href "https://alexandru-bg.github.io/bg-golden-layout/"
 npx ngh --dir=dist/bg-golden-layout
